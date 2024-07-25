93596cde633ce248f105c9751240e8e479a873ea
caitlinrsmith <catie12smith@gmail.com>
2024-07-25 15:06:06 -0500

    CaluculateBMI added

---
+++ CalculateBMI.js
@@ -0,0 +1,29 @@
+function bmi(weight, height) {
+    let bmiValue = weight / (height ** 2);
+      
+      if (bmiValue <= 18.5) {
+          return "Underweight";
+      } else if (bmiValue <= 25.0) {
+          return "Normal";
+      } else if (bmiValue <= 30.0) {
+          return "Overweight";
+      } else {
+          return "Obese";
+      }
+  }
+
+//   using Math.pow
+
+//   function bmi(weight, height) {
+//     let bmiValue = weight / Math.pow(height, 2);
+    
+//     if (bmiValue <= 18.5) {
+//         return "Underweight";
+//     } else if (bmiValue <= 25.0) {
+//         return "Normal";
+//     } else if (bmiValue <= 30.0) {
+//         return "Overweight";
+//     } else {
+//         return "Obese";
+//     }
+// }
