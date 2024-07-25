1b66415d5b78884a09e523183a0052c7b98bfe20
caitlinrsmith <catie12smith@gmail.com>
2024-07-25 16:00:13 -0500

    convert to string file added

---
+++ ConvertToSTring.js
@@ -0,0 +1,8 @@
+//www.codewars.com/kata/5265326f5fda8eb1160004c8/solutions/javascript
+
+//www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+function numberToString(num) {
+    let text = num.toString();
+    return text
+  }
