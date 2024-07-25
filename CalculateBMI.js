c0c6ea480995f0d611231a1b3e397261db6003bd
caitlinrsmith <catie12smith@gmail.com>
2024-07-25 15:08:45 -0500

    added mroe solutions to CalculateBMI

--- CalculateBMI.js
+++ CalculateBMI.js
@@ -27,3 +27,13 @@ function bmi(weight, height) {
 //         return "Obese";
 //     }
 // }
+
+// function bmi(weight, height) {
+  
+//     var bmi  = weight/(height*height);
+    
+//      return bmi < 18.5 ? "Underweight" : bmi <=25 ? "Normal" : bmi <= 30 ? "Overweight" : "Obese";
+    
+//     }
+
+// https://www.codewars.com/kata/57a429e253ba3381850000fb/solutions/javascript
